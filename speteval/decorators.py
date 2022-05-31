51f202e8e276e7b698f7ba95ffdfc288718709ee
Mahmoud Salhab <mahmoud@salhab.work>
2022-05-31 17:55:50 +0300

    decorators.py added

---
+++ speteval/decorators.py
@@ -0,0 +1,11 @@
+from exceptions import FileNotExist
+from functools import wraps
+import os
+
+
+def check_file_existance(func):
+    @wraps(func)
+    def wrapper(file_path, *args, **kwargs):
+        assert os.path.exists(file_path), FileNotExist(file_path)
+        return func(file_path, *args, **kwargs)
+    return wrapper
