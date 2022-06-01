572bf5f0387179c87c4824a978716f7567714bb6
Mahmoud Salhab <mahmoud@salhab.work>
2022-06-01 08:33:18 +0300

    utils.py added

---
+++ speteval/utils.py
@@ -0,0 +1,63 @@
+from typing import Callable, List, Tuple, Union
+from .constants import FilePath, AudioContent
+from .decorators import check_file_existance
+from torch import Tensor
+import numpy as np
+import torchaudio
+import os
+
+
+@check_file_existance
+def load_audio(audio_path: FilePath) -> Tuple[Tensor, int]:
+    x, sr = torchaudio.load(audio_path, channels_first=True)
+    return x, sr
+
+
+def _get_dim(content: AudioContent, operation: Callable) -> int:
+    length = len(content)
+    content = content[0]
+    while True:
+        try:
+            content = content[0]
+            length = operation(length, len(content))
+        except Exception:
+            break
+    return length
+
+
+def get_min_dim(content: AudioContent) -> int:
+    return _get_dim(content, min)
+
+
+def get_max_dim(content: AudioContent) -> int:
+    return _get_dim(content, max)
+
+
+def get_file_extension(file_path: FilePath) -> str:
+    return os.path.splitext(file_path)[-1].replace('.', '')
+
+
+def get_text_to_speech_ratio(
+        text: str, content: AudioContent, eps=1e-9
+        ) -> float:
+    max_dim = max(get_max_dim(content), eps)
+    return len(text) / max_dim
+
+
+def get_n_frames(content: AudioContent, hop_length: int) -> int:
+    n_samples = get_max_dim(content)
+    return int(n_samples) // hop_length
+
+
+def get_text_to_frame_ratio(
+        text: str, content: AudioContent, hop_length: int
+        ) -> float:
+    return len(text) / get_n_frames(content, hop_length)
+
+
+def get_std(values: List[Union[float, int]]) -> float:
+    return np.std(values)
+
+
+def get_mean(values: List[Union[float, int]]) -> float:
+    return np.mean(values)
